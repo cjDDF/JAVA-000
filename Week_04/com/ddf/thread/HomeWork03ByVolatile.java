3d04c2db18cfb319f5b50444c098a05df553660e
danyuanpeng <1575658489@qq.com>
2020-11-12 18:00:43 +0800

    第四周作业

---
+++ Week_04/com/ddf/thread/HomeWork03ByVolatile.java
@@ -0,0 +1,31 @@
+package com.ddf.thread;
+
+public class HomeWork03ByVolatile {
+    private volatile static Integer result;
+
+    public static void main(String[] args) throws InterruptedException {
+        long start = System.currentTimeMillis();
+        Object obj = new Object();
+
+        //异步运行
+        new Thread(() -> {
+            result = sum();
+        }).start();
+        //获取返回值
+        while (result == null) {
+        }
+        System.out.println("异步计算结果为：" + result);
+        System.out.println("使用时间：" + (System.currentTimeMillis() - start) + " ms");
+    }
+
+    private static int sum() {
+        return fibo(36);
+    }
+
+    private static int fibo(int a) {
+        if (a < 2) {
+            return 1;
+        }
+        return fibo(a - 1) + fibo(a - 2);
+    }
+}
